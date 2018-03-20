7aae62b10c2088485f0d0ea7d9a43dcbcfaf34ef
Shahriyar Sheikh <shahriyar.sheikh@mparsec.com>
2018-03-20 17:21:03 +0500

    added parameters in CXNcontract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 var CXNcontract = artifacts.require("./CXNcontract.sol");
+var StandardToken = artifacts.require("./token/ERC20/StandardToken.sol")
 
 module.exports = function (deployer) {
-    deployer.deploy(CXNcontract);
+    deployer.deploy(CXNcontract,1523426400, 1523926800, 0x627306090abab3a6e1400e9345bc60c78a8bef57, 1e6, StandardToken.new(), 1e7);
 }
