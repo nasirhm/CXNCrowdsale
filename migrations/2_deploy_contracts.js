eb1fc7aec5c5f19e0fb6b122929ae1ac3ecb53e0
Zeeshan Abbasi <zeeshan@mparsec.com>
2018-03-22 19:22:49 +0500

    Adjusted softcap

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,8 @@ var CxNtoken = artifacts.require("./CxNtoken.sol");
 module.exports = function (deployer) {
     deployer.deploy(CxNtoken, web3.eth.coinbase).then(function() {
 
-        let cap = web3.toWei(3, "ether" );
-        let goal = web3.toWei(1, "ether" );
+        let cap = web3.toWei(20000, "ether" );
+        let goal = web3.toWei(6000, "ether" );
         
         let timeNow = new Date();
 
@@ -13,7 +13,7 @@ module.exports = function (deployer) {
 
         let timestampExpiry = (expiry / 1000).toFixed(0);
 
-        return deployer.deploy(CxNcontract, 1521594000, timestampExpiry, web3.eth.coinbase, cap, CxNtoken.address, goal, web3.eth.accounts[3]);
+        return deployer.deploy(CxNcontract, 1521594000, 1526709600, web3.eth.coinbase, cap, CxNtoken.address, goal, web3.eth.accounts[3]);
     });
     
 }
